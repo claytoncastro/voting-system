160d59f31483076f32d8ca500defddc48737e525
claytoncastro <claytoncastro.ti@hotmail.com>
2023-03-10 14:03:19 -0300

    Criação do módulo gateway e registro do ms-cooperativa

---
+++ mscloudgateway/src/main/java/br/com/project/mscloudgateway/MscloudgatewayApplication.java
@@ -0,0 +1,28 @@
+package br.com.project.mscloudgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+@EnableEurekaClient
+@EnableDiscoveryClient
+@SpringBootApplication
+public class MscloudgatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MscloudgatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator routes(RouteLocatorBuilder builder) {
+		return builder
+				.routes()
+					.route(r -> r.path("/pautas/**").uri("lb://mscooperativa"))
+				.build();
+	}
+
+}
---
+++ mscloudgateway/src/test/java/br/com/project/mscloudgateway/MscloudgatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.project.mscloudgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MscloudgatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
